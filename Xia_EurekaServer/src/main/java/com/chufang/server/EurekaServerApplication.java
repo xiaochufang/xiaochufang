fa4a08f406f791e222f5b8b4433ae925e210eadc
mengbing <1223487561@qq.com>
2019-11-12 22:14:07 +0800

    下厨房模板3

---
+++ Xia_EurekaServer/src/main/java/com/chufang/server/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.chufang.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
